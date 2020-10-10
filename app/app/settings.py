5caadcd809f314f84026d5c310fa0ae16d80ee83
siaulys-kestutis <kestutis.siaulys.at@gmail.com>
2020-10-11 00:46:01 +0200

    Added a custom user model

--- app/app/settings.py
+++ app/app/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# The setting that refers to the actual class that is responsible for the user model
+# this is retrieved via the get_user_model method (see core/tests/test_models.py class)
+AUTH_USER_MODEL = 'core.User'
---
+++ app/core/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
+    PermissionsMixin
+
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Creates and saves a new user"""
+        if not email:
+            raise ValueError('User must have a valid email address')
+        # creates the new user model - this corresponds to the User class below
+        # NOT fully clear how atm
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        # sets the password
+        user.set_password(password)
+        # saves the model to some database
+        user.save(using=self._db)
+        # returns the just created user model
+        return user
+
+    # this is actually going to be used for the python manage.py createsuperuser command
+    def create_super_user(self, email, password):
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """Custom user model that supports using email instead of username"""
+    # these are all class fields - i.e. static variables
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BigIntegerField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    user_manager = UserManager()
+
+    USERNAME_FIELD = 'email'
+
