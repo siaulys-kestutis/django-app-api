d4674f965837a2782c8844ebee044144d84e6c14
siaulys-kestutis <kestutis.siaulys.at@gmail.com>
2020-10-11 01:56:58 +0200

    Set up Django admin for the custom user model.

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,48 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from core import models
+# the recommended convention for converting strings in Python to
+# human readable text - so that it gets passed through the translation
+# engine
+from django.utils.translation import gettext as _
+
 
 # Register your models here.
+class UserAdmin(BaseUserAdmin):
+    # ordering will be based on the id of the object
+    ordering = ['id']
+    # the ordering in which the table would get displayed?
+    list_display = ['email', 'name']
+    # without these fieldsets, the test: test_admin.test_user_change_page
+    # would fail with a message:
+    #
+    # django.core.exceptions.FieldError: Unknown field(s)
+    # (last_name, date_joined, first_name, username)
+    # specified for User.
+    # Check fields/fieldsets/exclude attributes of class UserAdmin.
+    fieldsets = (
+        (None, {'fields':  ('email', 'password')}),
+        (_('Personal Info'), {'fields': ('name',)}),
+        (
+            _('Permissions'),
+            {'fields': ('is_active', 'is_staff', 'is_superuser')}
+        ),
+        (_('Important dates'), {'fields': ('last_login',)})
+    )
+    # user admin by default takes add_fieldsets which defines the
+    # fields that you include on the add page (the same as the
+    # create user page)
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2')
+        }),
+    )
+
+
+# register our created User model as the UserAdmin model here
+#
+# i.e., specify which models are supposed to show up in the admin panel
+# this then allows to add entries (rows) to the db based on the model
+admin.site.register(models.User, UserAdmin)
+
---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,41 @@
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+
+class AdminSiteTest(TestCase):
+
+    # set up method that is run once before all the tests
+    def setUp(self):
+        self.client = Client()
+        self.admin_user = get_user_model().user_manager.create_super_user(email='admin@gmail.com', password='password')
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().user_manager.create_user(email='user@gmail.com', password='password', name='Test User')
+
+    def test_users_listed(self):
+        """Tests whenever the users are listed in the Django admin user page"""
+        url = reverse('admin:core_user_changelist')
+        # IMPORTANT: will use the test client to perform the HTTP GET on the url
+        response = self.client.get(url)
+
+        # assertContains - a Django custom assertion that checks that the response
+        # contains a certain item
+        # checks that the HTTP response was HTTP 200
+        self.assertContains(response, self.user.name)
+        self.assertContains(response, self.user.email)
+
+    def test_user_change_page(self):
+        """Test that the user edit page works"""
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        # reverse will create the following url: /admin/core/user/1
+        response = self.client.get(url)
+        # HTTP response code 200 means the response was "OK"
+        self.assertEqual(response.status_code, 200)
+
+    def test_create_user_page(self):
+        """Test that the create user page works"""
+        url = reverse('admin:core_user_add')
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 200)
+
