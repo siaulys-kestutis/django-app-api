e95bde4cbb05251eea6c07dfc2976715e785bde0
siaulys-kestutis <kestutis.siaulys.at@gmail.com>
2020-10-11 13:21:09 +0200

    Minor test fix

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -51,8 +51,8 @@ class CommandTests(TestCase):
     # patch replaces the behaviour of time.sleep with the function
     # that returns True
     # this is just to speed up the test
-    @patch('time.sleep', return_value=True)
-    def test_wait_for_db(self, ts):
+    #@patch('time.sleep', return_value=True)
+    def test_wait_for_db(self):
         """Test waiting for DB"""
         with patch('django.db.utils.ConnectionHandler.__getitem__') as gi:
             # the first five times one calls the __get_item
